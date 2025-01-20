271d69977ec0770074754b2738f865c86e1ecc01
Maryoomah <148255944+Maryoomah@users.noreply.github.com>
2025-01-20 10:23:26 +0100

    MGT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{css}', 
-    './output/**/*.{html,js,css}'  
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       transitionProperty: {
@@ -14,4 +11,3 @@ module.exports = {
   },
   plugins: [],
 };
-
